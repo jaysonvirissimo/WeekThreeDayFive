d076e86d79ad4a5efd656d47b9b29c9c01d58e0a
Jayson Virissimo <jayson.virissimo@asu.edu>
2014-10-24 15:46:41 -0700

    start where, where query, and where line

--- lib/02_searchable.rb
+++ lib/02_searchable.rb
@@ -3,7 +3,15 @@ require_relative '01_sql_object'
 
 module Searchable
   def where(params)
-    # ...
+
+  end
+
+  def where_query
+    "SELECT * FROM #{self.class.table_name} WHERE #{where_line}"
+  end
+
+  def where_line
+    ""
   end
 end
 
