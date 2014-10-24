bd51a67e19a053a557c93bd2d454501f38945430
Jayson Virissimo <jayson.virissimo@asu.edu>
2014-10-24 16:32:22 -0700

    complete searchable module

--- lib/02_searchable.rb
+++ lib/02_searchable.rb
@@ -3,18 +3,18 @@ require_relative '01_sql_object'
 
 module Searchable
   def where(params)
-
+    parse_all(DBConnection.execute(where_query(params), *params.values))
   end
 
-  def where_query
-    "SELECT * FROM #{self.class.table_name} WHERE #{where_line}"
+  def where_query(params)
+    "SELECT * FROM #{self.table_name} WHERE #{where_line(params)}"
   end
 
-  def where_line
-    ""
+  def where_line(params)
+    params.keys.map { |column| "#{column} = ?" }.join(" AND ")
   end
 end
 
 class SQLObject
-  # Mixin Searchable here...
+  extend Searchable
 end
